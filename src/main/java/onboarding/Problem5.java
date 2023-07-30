a1603afe01c53e2ac39b1fb4f4e5adb7ee0ab99e
이수민 <Smlee0109@naver.com>
2023-07-31 01:04:53 +0900

    feat(Problem5) : 돈 계산(완)

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,26 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>();
+
+        int[] m = new int[]{50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for(int i=0; i<m.length; i++){
+
+            int charge = Caculate(money, m[i]);
+            answer.add(charge);
+            money -= charge * m[i];
+
+        }
         return answer;
     }
 
-    private static List<Integer> Arr (int money){
-        int[] m = new int[9];
+    private static int Caculate(int money, int i){
+        return money/i;
     }
 }
